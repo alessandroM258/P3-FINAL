bda0873ba34f85ef591dd158dcf1760349f024cf
alessandroM258 <144828778+alessandroM258@users.noreply.github.com>
2024-12-18 06:53:17 +0000

    password generator

--- final.py
+++ final.py
@@ -1,1 +1,21 @@
+import random
 
+print('Password Generator')
+
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@$%^&*().,?0123456789'
+
+number = input('Amount of passwords to generate:')
+number = int(number)
+
+length = input('Input length of password:')
+length = int(length)
+
+print("These are your passwords. Make sure to remember them!")
+
+def generate_password(length, count):
+    passwords = []
+for pwd in range(number):
+    passwords = ''
+    for c in range(length):
+        passwords += random.choice(chars)
+    print (passwords)
